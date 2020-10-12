5afe0c3d66d1cddb84a4d99df2a32d9240cd0379
sparrow1116 <yingjiez1@126.com>
2020-10-12 21:24:27 +0800

    zyj remove config

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ let edit = require('./appApi/edit.js')
 const { logger, accessLogger } = require('./utils/log_config');
 
 const {sequelize} = require('./mysql/db')
+app.use(require('koa-static')(__dirname + '/public'))
 
 app.use(accessLogger());
 // console.log(enforceHttps)
@@ -30,7 +31,7 @@ app.use(accessLogger());
 app.use(bodyParser({
     enableTypes: ['json', 'form', 'text'], // 配置可解析的类型
   }));
-app.use(cors({
+app.use(cors(/*{
     origin: function (ctx) {
         return '*'  // 允许来自所有域名请求
        // return 'http://localhost:8080'; / 这样就能只允许 http://localhost:8080 这个域名的请求了
@@ -40,24 +41,24 @@ app.use(cors({
     credentials: true,
     allowMethods: ['GET', 'POST', 'DELETE', 'OPTIONS', 'PUT'],
     allowHeaders: ['Content-Type', 'Authorization', 'Accept'],
-}))
-app.use(async (ctx, next)=> {
-    ctx.set("Access-Control-Allow-Origin", "*");
-    ctx.set("Access-Control-Allow-Headers", "X-Requested-With");
-    ctx.set("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-    ctx.set("X-Powered-By",' 3.2.1');
-    ctx.set("Content-Type", "application/json;charset=utf-8");
+}*/))
+// app.use(async (ctx, next)=> {
+//     ctx.set("Access-Control-Allow-Origin", "*");
+//     ctx.set("Access-Control-Allow-Headers", "X-Requested-With");
+//     ctx.set("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+//     ctx.set("X-Powered-By",' 3.2.1');
+//     ctx.set("Content-Type", "application/json;charset=utf-8");
 
-    // ctx.set('Access-Control-Allow-Origin', '*');
-    // ctx.set('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
-    // ctx.set('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
-    if (ctx.method == 'OPTIONS') {
-        console.log('fuck')
-      ctx.body = 200; 
-    } else {
-      await next();
-    }
-  });
+//     // ctx.set('Access-Control-Allow-Origin', '*');
+//     // ctx.set('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+//     // ctx.set('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+//     if (ctx.method == 'OPTIONS') {
+//         console.log('fuck')
+//       ctx.body = 200; 
+//     } else {
+//       await next();
+//     }
+//   });
 
 //装载所有子路由
 let router = new Router()
@@ -89,8 +90,8 @@ app.on("error",err=>{
 })
 
 const options = {
-    key: fs.readFileSync('./ssh/4597038_www.yangmaoba.club.key'),
-    cert: fs.readFileSync('./ssh/4597038_www.yangmaoba.club.pem')
+    key: fs.readFileSync('./ssh/4599065_www.yangmaoba.club.key'),
+    cert: fs.readFileSync('./ssh/4599065_www.yangmaoba.club.pem')
 }
 
 https.createServer(options,app.callback()).listen(3000,()=>{
