3f032c8a27732910c8855fa32fa99d52a3aed352
sparrow1116 <yingjiez1@126.com>
2020-11-02 22:42:45 +0800

    zyj commit for change weixinqunpic

--- appApi/static.js
+++ appApi/static.js
@@ -11,7 +11,7 @@ router.post('/weixinqun',async(ctx)=>{
     // let data = ctx.request.body
     // console.log(TAG, JSON.stringify(data));
 	try{                                     
-		ctx.body={code:200,msg:{code:0,data:'https://bankdata.oss-cn-beijing.aliyuncs.com/wexinqun.png?v=2'}}	
+		ctx.body={code:200,msg:{code:0,data:'https://bankdata.oss-cn-beijing.aliyuncs.com/wexinqun.png?v=3'}}	
 	}catch(e){
         ctx.body = {code:200,msg:{code:1,data:'server error'}}
 	}
